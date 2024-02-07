686e97efebe071e7fb0c9fb234f9008c5ca2ee24
YuriQueirozAndrade <yuriqueirozandradeoficial@gmail.com>
2024-02-07 14:13:53 -0300

    Add Header component and style header.css

---
+++ Front-End/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css"
+export default function Header()
+{
+  return (
+  <header>
+      <nav className="nav-bar">
+        <input type="text" />
+        <button type="button"></button>
+      </nav>
+  </header>
+  )
+}
