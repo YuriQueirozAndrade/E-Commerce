1ab6f8451189b16612f195c246e1c19b478a11e1
YuriQueirozAndrade <yuriqueirozandradeoficial@gmail.com>
2024-02-07 13:39:02 -0300

    Add home-page component, and style in home.css

--- Front-End/src/pages/home/home-page.jsx
+++ Front-End/src/pages/home/home-page.jsx
@@ -1,8 +1,19 @@
+import Header from "@components/header/Header.jsx";
+import Footer from "@components/footer/Footer.jsx";
+import Modal from "@components/modal/Modal.jsx";
+import "./home.css";
 export default function Home()
 {
   return (
   <>
-    <h1>Hellow World!!!!</h1>
+    <Header />
+    <main className="mainContent">
+      <h1>Hellow World!!!!</h1>
+      <Modal />
+      <h1>Hellow World!!!!</h1>
+      <Modal />
+    </main>
+    <Footer />
   </>
   )
 }
