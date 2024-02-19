c69282dfbd9d2f03f4bae4b78f480cf309e1af51
YuriQueirozAndrade <yuriqueirozandradeoficial@gmail.com>
2024-02-19 05:26:52 -0300

    Add searchBar component

---
+++ Front-End/src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import "./bar.css"
+export default function Header()
+{
+  return (
+  <form className="form-search">
+    <input className="search-bar" type="text" placeholder="Search..." />
+    <button className="search-button" type="button"></button>
+  </form>
+  )
+}
+
