f68d8b9f876d3c95fc97aea87fd6e9d8fa3756cc
YuriQueirozAndrade <yuriqueirozandradeoficial@gmail.com>
2024-02-19 05:25:10 -0300

    Add buyBox component

---
+++ Front-End/src/components/buyBox/BuyBox.jsx
@@ -0,0 +1,19 @@
+import Modal from "@components/modal/Modal.jsx";
+import "@components/buyBox/box.css";
+export default function Box()
+{
+  return (
+    <Modal>
+      <div className="contentBox">
+        <main >
+        </main>
+        <footer className="footerBox">
+          <hr/>
+          <button type="button"></button>
+        </footer>
+      </div>
+    </Modal>
+  )
+}
+
+
