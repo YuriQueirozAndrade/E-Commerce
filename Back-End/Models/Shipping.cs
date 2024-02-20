9dfd7697c4716b9590d82510fb49914be93cf049
YuriQueirozAndrade <yuriqueirozandradeoficial@gmail.com>
2024-02-20 19:18:57 -0300

    Add the property Service to Shipping

--- Back-End/Models/Shipping.cs
+++ Back-End/Models/Shipping.cs
@@ -6,6 +6,7 @@ namespace Back_End.Models
     public class Shipping
     {
         public int Id { get; set; }
+        public string Service { get; set; }
         public decimal Cost { get; set; }
         public string TrackingNumber { get; set; }
         public DateTime? ShippedDate { get; set; }
