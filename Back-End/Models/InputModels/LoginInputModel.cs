88ca956bcd43fe9a21f37d6d5839eb28583f4806
Yuri Queiroz <yuriqueirozandradeoficial@gmail.com>
2024-03-03 09:04:44 -0300

    Add the Account InputModel

---
+++ Back-End/Models/InputModels/LoginInputModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+namespace Back_End.Models.InputModels
+{
+    public class LoginInputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+
+}
