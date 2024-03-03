550d238c3200b1f4118aac10193c9110b2cacff6
Yuri Queiroz <yuriqueirozandradeoficial@gmail.com>
2024-03-03 14:16:51 -0300

    Add  the IDeleted interface to user, and created data

--- Back-End/Models/User.cs
+++ Back-End/Models/User.cs
@@ -1,14 +1,16 @@
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
+using Back_End.Models.Interfaces;
 
 namespace Back_End.Models
 {
-  public class User : IdentityUser
+  public class User : IdentityUser, IDeletedEntity
   {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool Deleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
+
     public List<Address> Addresses { get; set; }
     public List<Order> Orders { get; set; }
   }
