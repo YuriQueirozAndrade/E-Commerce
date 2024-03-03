3802524cc88f258a69c868703c0572194581c8d4
Yuri Queiroz <yuriqueirozandradeoficial@gmail.com>
2024-03-03 14:19:17 -0300

    Add the migration of TPC

--- Back-End/Migrations/20240303165844_ChangeToTCPAprocch.Designer.cs
+++ Back-End/Migrations/20240303165844_ChangeToTCPAprocch.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Back_End.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Back_End.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240303165844_ChangeToTCPAprocch")]
+    partial class ChangeToTCPAprocch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
