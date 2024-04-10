1f5892a6c86f48238d5bb480e4034ed5038023a3
Yuri Queiroz <yuriqueirozandradeoficial@gmail.com>
2024-04-10 10:49:39 -0300

    Modify the GetTotalValue to catch diff index with two list

--- Back-End/E_Commerce_API/API.Application/Services/GetTotalValue.cs
+++ Back-End/E_Commerce_API/API.Application/Services/GetTotalValue.cs
@@ -1,18 +1,26 @@
-using E_Commerce_API.Application.Interfaces;
 using E_Commerce_API.Application.Interfaces.Services;
-namespace E_Commerce_API.Services
+
+namespace E_Commerce_API.Application.Services
 {
     public class GetTotalValue :IGetTotalValue
     {
-        public decimal CalculeteAmount(List<decimal> prices, List<int> quantity, decimal shipping)
+        public decimal CalculeteAmount (List<decimal> prices, List<int> quantity, decimal shipping)
         {
+            if (prices == null || quantity == null || prices.Count != quantity.Count)
+            {
+                throw new ArgumentException("Prices and quantity lists must be of equal length and not null.");
+            }
+
             decimal amount = 0;
+
             for (int i = 0; i < prices.Count; i++)
             {
-               amount += prices[i] * quantity[i];
+                amount += prices[i] * quantity[i];
             }
+
             amount += shipping;
             return amount;
         }
+
     }
 }
