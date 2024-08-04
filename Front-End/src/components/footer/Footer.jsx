21b545926143e6c39b8f51f7e5d6fd7c96c92f2b
Yuri <yuriqueirozandradeoficial@gmail.com>
2024-08-04 06:47:08 -0300

    Remove the Front-End and rename the Back-End to src

--- Front-End/src/components/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./footer.css";
-export default function Footer({children})
-{
-  return (
-    <footer className="default-footer">
-    {children}
-  </footer>
-  )
-}
--- Front-End/src/components/modal/Modal.jsx
+++
@@ -1,9 +0,0 @@
-import "./modal.css"
-export default function Modal({children, mWidth,mHeight,mBorder})
-{
-  return (
-  <div className="modal" style={{width: mWidth, height: mHeight, borderRadius: mBorder,}}>
-    {children}
-  </div>
-  )
-}
